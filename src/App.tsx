c75d4625e1538f606324b282ac4d8ce25e9d1422
lucashamaguchi <lucas@hamaguchi.com.br>
2024-01-29 12:02:43 -0300

    improve style on root

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,36 @@ import CityInput, {
 import WeatherStatus from "./components/WeatherStatus/WeatherStatus.component";
 import Geolocation from "./components/Geolocation/Geolocation.component";
 import styled from "styled-components";
+import Box from "@mui/material/Box";
 
 const CityInputStyle = styled.div`
-	margin: 30px auto;
+  margin: 30px auto;
 `;
 
 const GeolocationStyle = styled.div`
-	margin: 30px auto;
+  margin: 10px auto;
 `;
 
 const WeatherStatusStyle = styled.div`
-	margin: 30px auto;
+  margin: 30px auto;
 `;
 
 function App() {
   const [cityProps, setCityProps] = useState<ICityProperties | null>(null);
 
   return (
-    <div className="App">
-      <h1>
-        Amicci GetWeather App
-      </h1>
+    <Box
+      sx={{
+        margin: "50px",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+      }}
+    >
+      <h1 style={{
+        textAlign: 'center',
+      }}>Amicci GetWeather App</h1>
       <CityInputStyle>
         <CityInput onSelect={setCityProps} />
       </CityInputStyle>
@@ -36,7 +45,7 @@ function App() {
       <WeatherStatusStyle>
         <WeatherStatus cityProps={cityProps} />
       </WeatherStatusStyle>
-    </div>
+    </Box>
   );
 }
 
