bb29d760012de40c5a191add6bcccc1bbf30db6b
lucashamaguchi <lucas@hamaguchi.com.br>
2024-01-29 11:14:59 -0300

    format

--- src/services/getCityFromCoordinates.ts
+++ src/services/getCityFromCoordinates.ts
@@ -1,8 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
-async function getCityFromCoordinates(coords: {lat: number | null, lng: number | null}) {
-  const key = 'AIzaSyDtrsE6xhEUyV7feXRpDm3C11-XONrH7MM';
-  const response = await axios.get<any>(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=${key}`);
+async function getCityFromCoordinates(coords: {
+  lat: number | null;
+  lng: number | null;
+}) {
+  const key = "AIzaSyDtrsE6xhEUyV7feXRpDm3C11-XONrH7MM";
+  const response = await axios.get<any>(
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=${key}`
+  );
   return response.data;
 }
 
--- src/services/getCoordsFromCity.ts
+++ src/services/getCoordsFromCity.ts
@@ -1,8 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 async function getCoordsFromCity(address: string) {
-  const key = 'AIzaSyDtrsE6xhEUyV7feXRpDm3C11-XONrH7MM';
-  const response = await axios.get<any>(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${key}`);
+  const key = "AIzaSyDtrsE6xhEUyV7feXRpDm3C11-XONrH7MM";
+  const response = await axios.get<any>(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${key}`
+  );
   return response.data;
 }
 
