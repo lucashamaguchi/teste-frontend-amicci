1f33f49df7ca3798b4299298ec4c1c2e15a2baf1
lucashamaguchi <lucas@hamaguchi.com.br>
2024-01-29 12:02:00 -0300

    change metrics and add exclude on openweather integration

--- src/services/getWeatherFromCoords.ts
+++ src/services/getWeatherFromCoords.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 async function getWeather(coords: { lat: number | null; lng: number | null }) {
   const key = "49b8ff377a814e8cd0a8892e0cf14963";
   const response = await axios.get<any>(
-    `https://api.openweathermap.org/data/3.0/onecall?lat=${coords.lat}&lon=${coords.lng}&appid=${key}`
+    `https://api.openweathermap.org/data/3.0/onecall?lat=${coords.lat}&lon=${coords.lng}&appid=${key}&exclude=minutely,hourly,daily,weekly&units=metric`
   );
   return response.data;
 }
