73fc37daefdbab451a5daf1356dd08361596333f
lucashamaguchi <lucas@hamaguchi.com.br>
2024-01-29 12:02:30 -0300

    improve style and usability on weatherstatus component

--- src/components/WeatherStatus/WeatherStatus.component.tsx
+++ src/components/WeatherStatus/WeatherStatus.component.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import { ICityProperties } from "../CityInput/CityInput.component";
 import getWeatherFromCoords from "../../services/getWeatherFromCoords";
+import Box from "@mui/material/Box";
 
 export interface IWeatherStatusProps {
   cityProps: ICityProperties | null;
@@ -16,13 +17,38 @@ function WeatherStatusComponent(props: IWeatherStatusProps) {
     }
   };
 
+  useEffect(() => {
+    setWeather(null);
+  }, [props.cityProps?.coords?.lat, props.cityProps?.coords?.lng])
+
   return (
-    <div>
-      <Button variant="outlined" onClick={getWeather}>
-        Get Weather
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+
+      }}
+    >
+      <Button variant="outlined" disabled={!props.cityProps?.coords} onClick={getWeather}>
+        {props.cityProps?.name ?  `Get Weather for ${props.cityProps?.name}` : `Choose a city or use the geolocation`}
       </Button>
-      {JSON.stringify(weather)}
-    </div>
+      {weather && (
+        <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "space-between",
+  
+        }}>
+          <h2>{weather?.current.temp} °</h2>
+          <img
+            src={`http://openweathermap.org/img/w/${weather?.current.weather[0].icon}.png`}
+            alt={weather?.current.weather[0].description}
+          />
+        </Box>
+      )}
+    </Box>
   );
 }
 
